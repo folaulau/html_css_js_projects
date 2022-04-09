1ec100f8cc35eb17d1c44a8ca2bcaafbdfdf59d9
Folau Kaveinga <folaukaveinga@gmail.com>
2022-04-08 18:24:03 -0600

    added project 5

---
+++ project-5-variables/script.js
@@ -0,0 +1,23 @@
+
+// Store your name in a variable
+let name = "Folau"
+
+// Store your birth year in a variable
+const birthYear = 1986
+
+// Store a future year in a variable.
+const futureYear = 2022
+
+// Calculate your 2 possible ages for that year based on the stored values.
+
+// your age before turning to your new age
+const currentAge = (futureYear - birthYear) - 1;
+
+// your new age
+const newAge = (futureYear - birthYear);
+
+console.log("I will be either "+currentAge+" or "+newAge+" in "+futureYear)
+
+/**
+ * use your own birth date for testing.
+ */
